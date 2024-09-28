fdb3f0b2b7b628dd0ea58346432e2c3c421766e2
Sefik-Palazoglu <sefik.palazoglu.s@gmail.com>
2024-09-28 19:52:47 +0300

    feat: bootloader prototype

--- bootloader_0/boot.c
+++ bootloader_0/boot.c
@@ -1,5 +1,9 @@
 #include <avr/io.h>
 #include <avr/boot.h>
+#include <avr/wdt.h>
+
+void BOOTLOADER_SECTION Configure_Watchdog_Timer(uint8_t flags);
+uint8_t BOOTLOADER_SECTION Read_USART();
 
 void BOOTLOADER_SECTION Configure_Watchdog_Timer(uint8_t flags)
 {
@@ -7,15 +11,33 @@ void BOOTLOADER_SECTION Configure_Watchdog_Timer(uint8_t flags)
 	WDTCSR = flags;
 }
 
-void BOOTLOADER_SECTION Go_To_Application_Start(void)
+void BOOTLOADER_SECTION __attribute((noreturn)) Go_To_Application_Start(void)
 {
 	Configure_Watchdog_Timer(0x00); // ignore watchdog timeout
-	asm("eor r30, r30");
-	asm("eor r31, r31");
-	asm("ijmp");
+	// RunApp();
+	asm volatile (
+		"eor r30, r30\n"
+		"eor r31, r31\n"
+		"ijmp\n"
+	);
 }
 
-void BOOTLOADER_SECTION Init_Timer1(void)
+void BOOTLOADER_SECTION Init_UART(void)
 {
-	TCCR1B = (1 << CS12) | (1 << CS10);
+	UCSR0A = (1 << U2X0);
+	UCSR0B = ((1 << RXEN0) | (1 << TXEN0));
+	UCSR0C = ((1 << UCSZ01) | (1 << UCSZ00));
+	UBRR0L = 0x10;
+}
+
+uint8_t BOOTLOADER_SECTION Read_USART()
+{
+	while (UCSR0A & (1 << RXC0)) ;
+
+	if (!(UCSR0A & (1 << FE0)))
+	{
+		wdt_reset();
+	}
+
+	return UDR0;
 }
--- bootloader_0/boot_init.c
+++ bootloader_0/boot_init.c
@@ -1,19 +1,34 @@
 #include <avr/io.h>
 #include <avr/boot.h>
+#include <avr/wdt.h>
 
-void BOOTLOADER_SECTION Go_To_Application_Start(void);
-void BOOTLOADER_SECTION Init_Timer1(void);
+void BOOTLOADER_SECTION __attribute((noreturn)) Go_To_Application_Start(void);
+void BOOTLOADER_SECTION Init_UART(void);
+void BOOTLOADER_SECTION Configure_Watchdog_Timer(uint8_t flags);
+uint8_t BOOTLOADER_SECTION Read_USART();
 
-void BOOTLOADER_SECTION __attribute__ ((used)) my_bootloader(void)
+void BOOTLOADER_SECTION __attribute__ ((used)) _Noreturn my_bootloader(void)
 {
-	if (MCUSR & (1 << EXTRF))
+	asm volatile ("eor r1, r1");
+	uint8_t mcusr = MCUSR;
+	MCUSR = 0x00;
+
+	if (mcusr & (1 << EXTRF))
 	{
-		MCUSR = 0x00;
-		Init_Timer1();
+		Init_UART();
+		Configure_Watchdog_Timer((1 << WDE) | (1 << WDP2) | (1 << WDP1));
+		wdt_reset();
+		uint8_t usart_read_0 = Read_USART();
+		if (usart_read_0 == 0x41)
+		{
+			uint8_t usart_read_1 = Read_USART();
+		}
+		else
+		{
+		}
 	}
 	else
 	{
-		MCUSR = 0x00;
 		Go_To_Application_Start();
 	}
 }
