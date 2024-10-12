59c420c2842cfa06a4156dc20ad55e8a485332e0
Sefik-Palazoglu <sefik.palazoglu.s@gmail.com>
2024-10-12 19:52:29 +0300

    refactor: shorten instructions

--- bootloader_0/main.c
+++ bootloader_0/main.c
@@ -82,6 +82,50 @@ void Read_N_Characters(uint8_t count)
     );                                           \
 }))
 
+#define boot_page_write_small(address)        \
+(__extension__({                                 \
+    __asm__ __volatile__                         \
+    (                                            \
+        "out %0, %1\n\t"                         \
+        "spm\n\t"                                \
+        :                                        \
+        : "i" (_SFR_IO_ADDR(__SPM_REG)),        \
+          "r" ((uint8_t)(__BOOT_PAGE_WRITE)),    \
+          "z" ((uint16_t)(address))              \
+    );                                           \
+}))
+
+#define boot_rww_enable_small()                      \
+(__extension__({                                 \
+    __asm__ __volatile__                         \
+    (                                            \
+        "out %0, %1\n\t"                         \
+        "spm\n\t"                                \
+        :                                        \
+        : "i" (_SFR_IO_ADDR(__SPM_REG)),        \
+          "r" ((uint8_t)(__BOOT_RWW_ENABLE))     \
+    );                                           \
+}))
+
+#define boot_page_fill_small(address, data)   \
+(__extension__({                                 \
+    __asm__ __volatile__                         \
+    (                                            \
+        "movw  r0, %3\n\t"                       \
+        "out %0, %1\n\t"                         \
+        "spm\n\t"                                \
+        "clr  r1\n\t"                            \
+        :                                        \
+        : "i" (_SFR_IO_ADDR(__SPM_REG)),        \
+          "r" ((uint8_t)(__BOOT_PAGE_FILL)),     \
+          "z" ((uint16_t)(address)),             \
+          "r" ((uint16_t)(data))                 \
+        : "r0"                                   \
+    );                                           \
+}))
+
+
+
 uint8_t* ram_buffer = (uint8_t*) RAMSTART;
 
 void __attribute__ ((used)) _Noreturn __attribute__ ((section(".text.my_bootloader"))) main(void)
@@ -147,11 +191,11 @@ void __attribute__ ((used)) _Noreturn __attribute__ ((section(".text.my_bootload
 				for (uint8_t i = 0; i < SPM_PAGESIZE; i += 2)
 				{
 					uint16_t data = (uint16_t) ram_buffer[i] | ((uint16_t) ram_buffer[i + 1] << 8);
-					boot_page_fill(cursor + i, data);
+					boot_page_fill_small(cursor + i, data);
 				}
-				boot_page_write(cursor);
+				boot_page_write_small(cursor);
 				boot_spm_busy_wait();
-				boot_rww_enable();
+				boot_rww_enable_small();
 			}
 			else if (usart_read_0 == 0x74)
 			{
