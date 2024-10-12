437821482d6a750f53ca343a7bb5ea692231282d
Sefik-Palazoglu <sefik.palazoglu.s@gmail.com>
2024-10-12 22:34:33 +0300

    refactor: boot and boot page functions

--- bootloader_0/main.c
+++ bootloader_0/main.c
@@ -1,7 +1,7 @@
 #include <avr/io.h>
-#include <avr/boot.h>
 #include <avr/wdt.h>
 #include <avr/pgmspace.h>
+#include "small_boot.h"
 
 void Configure_Watchdog_Timer(uint8_t flags)
 {
@@ -68,63 +68,6 @@ void Read_N_Characters(uint8_t count)
 		count -= 1;
 	}
 }
-
-#define boot_page_erase_small(address)        \
-(__extension__({                                 \
-    __asm__ __volatile__                         \
-    (                                            \
-        "out %0, %1\n\t"                         \
-        "spm\n\t"                                \
-        :                                        \
-        : "i" (_SFR_IO_ADDR(__SPM_REG)),        \
-          "r" ((uint8_t)(__BOOT_PAGE_ERASE)),    \
-          "z" ((uint16_t)(address))              \
-    );                                           \
-}))
-
-#define boot_page_write_small(address)        \
-(__extension__({                                 \
-    __asm__ __volatile__                         \
-    (                                            \
-        "out %0, %1\n\t"                         \
-        "spm\n\t"                                \
-        :                                        \
-        : "i" (_SFR_IO_ADDR(__SPM_REG)),        \
-          "r" ((uint8_t)(__BOOT_PAGE_WRITE)),    \
-          "z" ((uint16_t)(address))              \
-    );                                           \
-}))
-
-#define boot_rww_enable_small()                      \
-(__extension__({                                 \
-    __asm__ __volatile__                         \
-    (                                            \
-        "out %0, %1\n\t"                         \
-        "spm\n\t"                                \
-        :                                        \
-        : "i" (_SFR_IO_ADDR(__SPM_REG)),        \
-          "r" ((uint8_t)(__BOOT_RWW_ENABLE))     \
-    );                                           \
-}))
-
-#define boot_page_fill_small(address, data)   \
-(__extension__({                                 \
-    __asm__ __volatile__                         \
-    (                                            \
-        "movw  r0, %3\n\t"                       \
-        "out %0, %1\n\t"                         \
-        "spm\n\t"                                \
-        "clr  r1\n\t"                            \
-        :                                        \
-        : "i" (_SFR_IO_ADDR(__SPM_REG)),        \
-          "r" ((uint8_t)(__BOOT_PAGE_FILL)),     \
-          "z" ((uint16_t)(address)),             \
-          "r" ((uint16_t)(data))                 \
-        : "r0"                                   \
-    );                                           \
-}))
-
-
 
 uint8_t* ram_buffer = (uint8_t*) RAMSTART;
 
