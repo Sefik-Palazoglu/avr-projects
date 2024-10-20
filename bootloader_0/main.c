ea2f209a7ced566157bd82efdde2b09f5b8c63a6
Sefik-Palazoglu <sefik.palazoglu.s@gmail.com>
2024-10-20 21:10:48 +0300

    refactor: command.h

--- bootloader_0/main.c
+++ bootloader_0/main.c
@@ -3,6 +3,7 @@
 #include <avr/pgmspace.h>
 #include "small_boot.h"
 #include "uart.h"
+#include "command.h"
 
 void Configure_Watchdog_Timer(uint8_t flags);
 void _Noreturn Go_To_Application_Start(void);
@@ -29,46 +30,46 @@ void bootloader_func(void)
 		while (1)
 		{
 			uint8_t command = Read_USART();
-			if (command == 0x41) {
+			if (command == Cmnd_STK_GET_PARAMETER) {
 
-				uint8_t usart_read_1 = Read_USART();
+				uint8_t parameter = Read_USART();
 				synchronize_with_stk500();
-				if (usart_read_1 == 0x81 || usart_read_1 == 0x82)
+				if (parameter == Parm_STK_SW_MAJOR || parameter == Parm_STK_SW_MINOR)
 					Write_USART(0x04);
 				else
 					Write_USART(0x03);
 			}
-			else if (command == 0x42) {
+			else if (command == Cmnd_STK_SET_DEVICE) {
 
 				Read_N_Characters(0x14);
 				synchronize_with_stk500();
 			}
-			else if (command == 0x45) {
+			else if (command == Cmnd_SET_DEVICE_EXT) {
 
 				Read_N_Characters(0x05);
 				synchronize_with_stk500();
 			}
-			else if (command == 0x55) {
+			else if (command == Cmnd_STK_LOAD_ADDRESS) {
 
 				cursor = Read_USART() | ((uint16_t) Read_USART() << 8);
 				cursor *= 2;
 				synchronize_with_stk500();
 			}
-			else if (command == 0x56) {
+			else if (command == Cmnd_STK_UNIVERSAL) {
 
 				Read_N_Characters(0x04);
 				synchronize_with_stk500();
 				Write_USART(0x00);
 			}
-			else if (command == 0x64) {
+			else if (command == Cmnd_STK_PROG_PAGE) {
 
-				Read_USART();
-				uint8_t program_byte_count = Read_USART();
-				Read_USART();
+				uint8_t __attribute ((unused)) bytes_high = Read_USART();
+				uint8_t bytes_low = Read_USART();
+				uint8_t __attribute ((unused)) memtype = Read_USART();
 
 				boot_page_erase_small(cursor);
 
-				for (uint8_t i = 0; i < program_byte_count; i++)
+				for (uint8_t i = 0; i < bytes_low; i++)
 				{
 					ram_buffer[i] = Read_USART();
 				}
@@ -86,7 +87,7 @@ void bootloader_func(void)
 				boot_spm_busy_wait();
 				boot_rww_enable_small();
 			}
-			else if (command == 0x74) {
+			else if (command == Cmnd_STK_READ_PAGE) {
 
 				Read_USART();
 				uint8_t program_byte_count = Read_USART();
@@ -98,14 +99,14 @@ void bootloader_func(void)
 					program_byte_count--;
 				} while (0 != program_byte_count);
 			}
-			else if (command == 0x75) {
+			else if (command == Cmnd_STK_READ_SIGN) {
 
 				synchronize_with_stk500();
 				Write_USART(0x1E);
 				Write_USART(0x95);
 				Write_USART(0x0F);
 			}
-			else if (command == 0x51) {
+			else if (command == Cmnd_STK_LEAVE_PROGMODE) {
 
 				Configure_Watchdog_Timer(0x08);
 				synchronize_with_stk500();
@@ -115,7 +116,7 @@ void bootloader_func(void)
 				synchronize_with_stk500();
 			}
 
-			Write_USART(0x10);
+			Write_USART(Resp_STK_OK);
 		}
 	}
 	else {
@@ -146,9 +147,9 @@ void _Noreturn Go_To_Application_Start(void)
 void synchronize_with_stk500()
 {
 	uint8_t data = Read_USART();
-	if (data == 0x20)
+	if (data == Sync_CRC_EOP)
 	{
-		Write_USART(0x14);
+		Write_USART(Resp_STK_INSYNC);
 	}
 	else
 	{
