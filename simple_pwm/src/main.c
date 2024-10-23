ee1676de571c24757cf0ca9058fae4c7f776f764
Sefik-Palazoglu <sefik.palazoglu.s@gmail.com>
2024-10-23 22:15:03 +0300

    feat: drive a HC-SR04

--- simple_pwm/src/main.c
+++ simple_pwm/src/main.c
@@ -1,9 +1,12 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include "uart.h"
 
 int main(void)
 {
+  init_uart();
   // set PB5 LED as output
   DDRB |= _BV(PORTB5);
 
@@ -18,14 +21,20 @@ int main(void)
 
   // interrupt every 48 microseconds
 
+  // activate noise canceller
+  // select positive edge input capture
+  // start timer with prescaler == 1
+  TCCR1B = _BV(ICNC1) | _BV(ICES1) | _BV(CS11) | _BV(CS10);
+
   // enable interrupts
   sei();
   // enable timer 0 interrupt OCA interrupts
   TIMSK0 = _BV(OCIE0A);
+  // enable timer1 input capture interrupt
+  TIMSK1 = _BV(ICIE1);
 
   while (1)
   {
-    ;
   }
 }
 
@@ -42,5 +51,40 @@ ISR(TIMER0_COMPA_vect)
   else if (1 == x)
   {
     PORTB &= ~_BV(PORTB5);
+  }
+}
+
+ISR(TIMER1_CAPT_vect)
+{
+  static uint8_t capture_falling = 0;
+  static uint16_t capture_value = 0;
+  static char sending_char[5];
+
+  uint16_t capture_value_old = capture_value;
+  capture_value = ICR1L;
+  capture_value |= (ICR1H << 8);
+
+  if (capture_falling)
+  {
+    // capture rising edge next
+    TCCR1B |= _BV(ICES1);
+
+    capture_falling = 0;
+
+    uint16_t captured_pulse_width = capture_value - capture_value_old;
+    utoa(captured_pulse_width, sending_char, 10);
+    write_usart(sending_char[0]);
+    write_usart(sending_char[1]);
+    write_usart(sending_char[2]);
+    write_usart(sending_char[3]);
+    write_usart(sending_char[4]);
+    write_usart('\r');
+    write_usart('\n');
+  }
+  else
+  {
+    // capture falling edge next
+    TCCR1B &= ~_BV(ICES1);
+    capture_falling = 1;
   }
 }
